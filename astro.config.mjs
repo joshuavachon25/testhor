9c687b2e4e887d9d69dbc1118f7c2cc413c2b312
Joshua L.J. Vachon <joshua.vachon@cyberfjord.studio>
2023-12-11 23:33:46 -0500

    Init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,19 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  i18n: {
+    defaultLocale: "fr",
+    locales: ["en", {
+      path: "fr", // no slashes included
+      codes: ["fr", "fr-BR", "fr-CA"]
+    }],
+    routing: {
+      prefixDefaultLocale: true
+    }
+  },
+
+});
