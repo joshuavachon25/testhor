ac58ed1e6565bf1e05ded91fb2c4476707c51478
Joshua L.J. Vachon <joshua.vachon@cyberfjord.studio>
2024-02-11 20:11:15 -0500

    Blog post

--- src/utils/directus.js
+++ src/utils/directus.js
@@ -1,9 +1,11 @@
 import { createDirectus, rest, staticToken } from "@directus/sdk";
 
 export const getDirectusClient = async () => {
-	const client = createDirectus(import.meta.env.PUBLIC_DIRECTUS_URL)
-		.with(staticToken(import.meta.env.DIRECTUS_TOKEN))
-		.with(rest())
+  const client = createDirectus(import.meta.env.PUBLIC_DIRECTUS_URL)
+    .with(staticToken(import.meta.env.DIRECTUS_TOKEN))
+    .with(rest())
 
-	return client;
+  return client;
 };
+
+//https://docs.directus.io/guides/sdk/getting-started.html
